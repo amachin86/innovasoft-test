2fa78c95f0294349d5ad217a915452f710be18b3
amachin <git-commit-address>
2025-02-24 12:48:21 -0500

    fix: login page

--- client-maintenance/src/pages/Login.tsx
+++ client-maintenance/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { TextField, Button, Checkbox, FormControlLabel, Container, Typography, Box, Alert } from '@mui/material';
+import { TextField, Button, Checkbox, FormControlLabel, Container, Typography, Box, Alert, InputAdornment, IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import axios from 'axios';
 
 function LoginPage() {
@@ -8,6 +9,7 @@ function LoginPage() {
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar contraseña
   const navigate = useNavigate();
 
   const handleLogin = async () => {
@@ -52,22 +54,36 @@ function LoginPage() {
         />
         <TextField
           fullWidth
-          type="password"
           label="Contraseña"
           margin="normal"
+          type={showPassword ? 'text' : 'password'} // Cambia el tipo según el estado
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)} // Alterna el estado de mostrar/ocultar
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
-        <FormControlLabel
-          control={<Checkbox checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />}
-          label="Recuérdame"
-        />
+         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', mt: 1 }}>
+          <FormControlLabel
+            control={<Checkbox checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />}
+            label="Recuérdame"
+          />
+        </Box>
         <Button fullWidth variant="contained" color="primary" onClick={handleLogin} sx={{ mt: 2 }}>
           Iniciar Sesión
         </Button>
         <Typography variant="body2" align="center" style={{ marginTop: '16px' }} color="secondary">
-                ¿No tienes una cuenta?{' '}
-                <Link to="/register">Regístrate aquí</Link>
+          ¿No tienes una cuenta?{' '}
+          <Link to="/register">Regístrate aquí</Link>
         </Typography>
       </Box>
     </Container>
